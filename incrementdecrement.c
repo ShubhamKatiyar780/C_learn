1b64b3ba440f3925a2799fa0a8e8455013ae3003
SHUBHAM KATIYAR <168412256+ShubhamKatiyar780@users.noreply.github.com>
2025-01-14 19:21:56 +0530

    Add files via upload

---
+++ incrementdecrement.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+int x= 54;
+printf("%d\n",x);
+x++ ; // x = x+1 = 55
+printf("%d\n",x);
+x = x*8; //x=440
+printf("%d\n",x);
+x-- ; //x=439
+printf("%d\n",x);
+    return 0;
+}
